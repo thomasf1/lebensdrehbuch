10d78de09aa33335d61bd869ff4f0a98e8a0ad34
Thomas F <thomas@furch.com>
2025-07-05 10:37:53 +0200

    Added Google

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -4,6 +4,8 @@ import {
   wrapLanguageModel,
 } from 'ai';
 import { xai } from '@ai-sdk/xai';
+import { google } from '@ai-sdk/google'
+
 import {
   artifactModel,
   chatModel,
@@ -23,13 +25,13 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
+        'chat-model': google('gemini-2.5-flash-preview-04-17'), //xai('grok-2-vision-1212'),
         'chat-model-reasoning': wrapLanguageModel({
           model: xai('grok-3-mini-beta'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        'title-model': google('gemini-2.0-flash-lite-preview-02-05'), // xai('grok-2-1212'),
+        'artifact-model': google('gemini-2.5-flash-preview-04-17'), //xai('grok-2-1212'),
       },
       imageModels: {
         'small-model': xai.imageModel('grok-2-image'),
